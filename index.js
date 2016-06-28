61329764bfacc3febd0eaf7469a329f954365574
Ruen Zapanta <rzapanta@stratpoint.com>
2016-06-28 16:53:09 +0800

    Added materialize-css

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import 'babel-polyfill'
+import 'materialize-css/dist/css/materialize.css'
+import 'materialize-css/dist/js/materialize.js'
+
 import React from 'react'
 import { render } from 'react-dom'
 import Quiz from './components/Quiz'
--- webpack.config.js
+++ webpack.config.js
@@ -13,13 +13,24 @@ var config = {
         test: /\.jsx?/,
         exclude: /(node_modules)/,
         loader: 'babel'
-      }
+      },
+      { test: /\.css$/, loader: "style-loader!css-loader" },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file?name=../dist/fonts/[hash].[ext]" },
+      { test: /\.(woff|woff2)$/, loader:"url?prefix=font/&limit=5000&name=../dist/fonts/[hash].[ext]" },
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream&name=../dist/fonts/[hash].[ext]" },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml&name=./[hash].[ext]" }
     ]
   },
   resolve: {
     modulesDirectories: ['node_modules'],
     extensions: ['', '.js', '.jsx']
-  }
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery"
+    })
+  ]
 };
 
 module.exports = config;
