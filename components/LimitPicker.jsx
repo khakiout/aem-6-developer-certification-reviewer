92f82b486d0acecbbf744930148f722faedd5061
Ruen Zapanta <rzapanta@stratpoint.com>
2016-07-29 19:52:10 +0800

    #9 - Moved materialize_select initialization to LimitPicker component

--- components/LimitPicker.jsx
+++ components/LimitPicker.jsx
@@ -1,31 +1,39 @@
 import React from 'react'
 
-const LimitPicker = ({
-  onSubmit
-}) => {
+export default class Answer extends React.Component {
+
+  constructor(props) {
+      super(props)
+  }
+
+  componentDidMount() {
+    // is this the right way to do this?
+    let me = $(this.refs.itemcount)
+    me.material_select();
+  }
+
+  render() {
+    const {
+      onSubmit
+    } = this.props
+
   return (
     <div className="card-panel">
-      <div className="row">
-        <form className="col s12">
-          <div className="row">
-            <div className="input-field col s3">
-              <select id="itemcount" name="itemcount">
-                <option>5</option>
-                <option>10</option>
-                <option>15</option>
-                <option>20</option>
-                <option>30</option>
-              </select>
-              <label htmlFor="itemcount">Materialize Select</label>
-            </div>
+          <div className="input-field col s3">
+            <select ref="itemcount" id="itemcount" name="itemcount">
+              <option>5</option>
+              <option>10</option>
+              <option>15</option>
+              <option>20</option>
+              <option>30</option>
+            </select>
+            <label htmlFor="itemcount">Enter number of questions</label>
           </div>
-          <div className="row">
+          
+          <div className="input-field col s3">
             <button onClick={onSubmit} className="waves-effect waves-light btn">Start</button>
           </div>
-        </form>
-      </div>
     </div>
   )
 }
-
-export default LimitPicker
+}
--- index.js
+++ index.js
@@ -3,7 +3,6 @@ import 'materialize-css/dist/css/materialize.css'
 import './style/main.less'
 import './style/print.less'
 import 'materialize-css/dist/js/materialize.js'
-import './style/main.js'
 
 import React from 'react'
 import { render } from 'react-dom'
