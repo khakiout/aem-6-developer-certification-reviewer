f4c154b17b7b213b07d9147be1437e116bcaeab2
Ruen Zapanta <rzapanta@stratpoint.com>
2016-08-02 18:46:16 +0800

    Some tab adjustments

--- components/LimitPicker.jsx
+++ components/LimitPicker.jsx
@@ -20,21 +20,21 @@ export default class LimitPicker extends React.Component {
     return (
       <div className="card-panel">
         <div className="row">
-        <div className="input-field col s6">
-          <select ref="itemcount" id="itemcount" name="itemcount" defaultValue="5" >
-            <option value="5">5</option>
-            <option value="10">10</option>
-            <option value="15">15</option>
-            <option value="20">20</option>
-            <option value="30">30</option>
-          </select>
-          <label htmlFor="itemcount">Enter number of questions</label>
+          <div className="input-field col s6">
+            <select ref="itemcount" id="itemcount" name="itemcount" defaultValue="5" >
+              <option value="5">5</option>
+              <option value="10">10</option>
+              <option value="15">15</option>
+              <option value="20">20</option>
+              <option value="30">30</option>
+            </select>
+            <label htmlFor="itemcount">Enter number of questions</label>
+          </div>
+          
+          <div className="input-field col s3">
+            <button onClick={(count) => onSubmit(this.refs.itemcount.value)} className="waves-effect waves-light btn">Start</button>
+          </div>
         </div>
-        
-        <div className="input-field col s3">
-          <button onClick={(count) => onSubmit(this.refs.itemcount.value)} className="waves-effect waves-light btn">Start</button>
-        </div>
-      </div>
       </div>
     )
   }
